d0d073d476ce6806bd9c0bf01554dd193c45adfb
Dalia <43527456+dollybuzz@users.noreply.github.com>
2019-11-27 06:34:41 +0000

    integrated getRandomImages functions into separate file, tools.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.static("public"));
 
 const request = require("request");
 const mysql = require("mysql");
+const tools = require("./tools.js");
 
 //routes
 //root route
@@ -27,48 +28,27 @@ app.get("/", function(req, res) {
     });//request
 });//root route
 
-app.get("/search", function(req, res) { 
-    //console.dir(req);
+app.get("/search", async function(req, res) { //'async' is for Promise method
+    //console.dir(req); //allows us to see all properties
     //console.log(req.query.keyword); //keyword is the name of the parameter passed in the form
     var keyword = req.query.keyword;
+    
     //the next line will NOT work for asynchronous functions, the imageURLs variable will be undefined
     //var imageURLs = getRandomImages(keyword, 9);
     
-    getRandomImages_cb(keyword, 9, function(imageURLs){
+    //CALLBACK METHOD
+    /*getRandomImages_cb(keyword, 9, function(imageURLs){
         console.log("imageURLS: " + imageURLs);
         res.render("results.ejs", {"results": imageURLs});
-    })
+    })*/
     
+    //PROMISE METHOD
+    var imageURLs= await tools.getRandomImages(keyword, 9); //requires 'async' word before function on line 30
+    console.log("imageURLS using Promises: " + imageURLs);
+    res.render("results.ejs", {"results": imageURLs});
+        
 });//search
 
-/*
-Return random image URLs from an API
-@param string keyword - search tearm
-@param int imageCount - number of random images
-@return array of image URLs
-*/
-function getRandomImages_cb(keyword, imageCount, callback){
-    var requestURL = "https://api.unsplash.com/photos/random?query="+keyword+"&count="+imageCount+"&client_id=079b4e9f05b286d943c3d8eb4ba519395d27a2ba8c556c58213d8275234fd396&orientation=landscape";
-    request(requestURL, function(error, response, body) {
-        if (!error) {
-            var parsedData = JSON.parse(body);
-            //console.log("image url:", parsedData["urls"]["regular"]);
-            var imageURLs = [];
-            
-            for(let i = 0; i < 9; i++)
-            {
-                imageURLs.push(parsedData[i].urls.regular);
-            }
-            //console.log(imageURLs);
-            
-            //return imageURLs;
-            callback(imageURLs);
-            
-        } else {
-            console.log("error", error);
-        }
-    });//request
-}
 
 //server listener
 app.listen("8080", "127.0.0.1", function() {
---
+++ tools.js
@@ -0,0 +1,68 @@
+const request = require("request");
+
+module.exports = {
+/*
+Return random image URLs from an API
+@param string keyword - search tearm
+@param int imageCount - number of random images
+@return array of image URLs
+*/
+//function getRandomImages_cb(keyword, imageCount, callback){
+getRandomImages_cb: function (keyword, imageCount, callback){
+    var requestURL = "https://api.unsplash.com/photos/random?query="+keyword+"&count="+imageCount+"&client_id=079b4e9f05b286d943c3d8eb4ba519395d27a2ba8c556c58213d8275234fd396&orientation=landscape";
+    request(requestURL, function(error, response, body) {
+        if (!error) {
+            var parsedData = JSON.parse(body);
+            //console.log("image url:", parsedData["urls"]["regular"]);
+            var imageURLs = [];
+            
+            for(let i = 0; i < 9; i++)
+            {
+                imageURLs.push(parsedData[i].urls.regular);
+            }
+            //console.log(imageURLs);
+            
+            //return imageURLs;
+            callback(imageURLs);
+            
+        } else {
+            console.log("error", error);
+        }
+    });//request
+    
+}, //requires comma after function
+
+/*
+Return random image URLs from an API
+@param string keyword - search tearm
+@param int imageCount - number of random images
+@return array of image URLs
+*/
+//function getRandomImages_promise(keyword, imageCount){
+getRandomImages: function (keyword, imageCount){
+    var requestURL = "https://api.unsplash.com/photos/random?query="+keyword+"&count="+imageCount+"&client_id=079b4e9f05b286d943c3d8eb4ba519395d27a2ba8c556c58213d8275234fd396&orientation=landscape";
+    
+    return new Promise( function(resolve, reject) {
+        request(requestURL, function(error, response, body) {
+        if (!error) {
+            var parsedData = JSON.parse(body);
+            //console.log("image url:", parsedData["urls"]["regular"]);
+            var imageURLs = [];
+            
+            for(let i = 0; i < 9; i++)
+            {
+                imageURLs.push(parsedData[i].urls.regular);
+            }
+            //console.log(imageURLs);
+            
+            //return imageURLs;
+            resolve(imageURLs);
+            
+        } else {
+            console.log("error", error);
+        }
+    });//request
+    });//promise
+}//function but no comma necessary
+
+}//end module.exports
