9c20ec7444f75c1bf8ceecd3621690a1640c74ef
Dalia <43527456+dollybuzz@users.noreply.github.com>
2019-11-27 06:14:32 +0000

    added callback function

--- app.js
+++ app.js
@@ -31,7 +31,24 @@ app.get("/search", function(req, res) {
     //console.dir(req);
     //console.log(req.query.keyword); //keyword is the name of the parameter passed in the form
     var keyword = req.query.keyword;
-    var requestURL = "https://api.unsplash.com/photos/random?query="+keyword+"&count=9&client_id=079b4e9f05b286d943c3d8eb4ba519395d27a2ba8c556c58213d8275234fd396&orientation=landscape";
+    //the next line will NOT work for asynchronous functions, the imageURLs variable will be undefined
+    //var imageURLs = getRandomImages(keyword, 9);
+    
+    getRandomImages_cb(keyword, 9, function(imageURLs){
+        console.log("imageURLS: " + imageURLs);
+        res.render("results.ejs", {"results": imageURLs});
+    })
+    
+});//search
+
+/*
+Return random image URLs from an API
+@param string keyword - search tearm
+@param int imageCount - number of random images
+@return array of image URLs
+*/
+function getRandomImages_cb(keyword, imageCount, callback){
+    var requestURL = "https://api.unsplash.com/photos/random?query="+keyword+"&count="+imageCount+"&client_id=079b4e9f05b286d943c3d8eb4ba519395d27a2ba8c556c58213d8275234fd396&orientation=landscape";
     request(requestURL, function(error, response, body) {
         if (!error) {
             var parsedData = JSON.parse(body);
@@ -43,12 +60,15 @@ app.get("/search", function(req, res) {
                 imageURLs.push(parsedData[i].urls.regular);
             }
             //console.log(imageURLs);
-            res.render("results.ejs", {"results": imageURLs});
+            
+            //return imageURLs;
+            callback(imageURLs);
+            
         } else {
-            res.render("results.ejs", {"error": "Unable to access API"});
+            console.log("error", error);
         }
     });//request
-});//search
+}
 
 //server listener
 app.listen("8080", "127.0.0.1", function() {
