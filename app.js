f255455aa66e3a52eba8e0ada858fa05353e9767
Dalia <43527456+dollybuzz@users.noreply.github.com>
2019-11-29 03:37:50 +0000

    added procfile and heroku server listener

--- app.js
+++ app.js
@@ -110,6 +110,11 @@ app.get("/api/displayFavorites", function(req, res) {
 });//displayFavorites route
 
 //server listener
-app.listen("8080", "127.0.0.1", function() {
+/*app.listen("8080", "127.0.0.1", function() {
+    console.log("Running Express Server...");
+});*/
+
+//server listener
+app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Running Express Server...");
 });
